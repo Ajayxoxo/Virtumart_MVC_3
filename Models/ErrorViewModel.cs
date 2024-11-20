51c5de327fef1a89ae0865de9dba626dff19ae3a
Ajayxoxo <ajaysekar40130@gmail.com>
2024-11-20 20:00:28 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Virtumart_MVC_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
