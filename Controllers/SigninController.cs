277a275737b9069a01125d66f7b768bd643ca9ba
Ajayxoxo <ajaysekar40130@gmail.com>
2024-11-23 18:54:07 +0530

    Adding Signin page for User

---
+++ Controllers/SigninController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Virtumart_MVC_3.Models;
+
+namespace Virtumart_MVC_3.Controllers
+{
+    public class SigninController : Controller
+    {
+        private readonly VirtuMartContext _context;
+
+        public SigninController(VirtuMartContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Signin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+            public IActionResult Signin(SigninModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.userinfo.FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password );
+                if (user == null)
+                {
+                    ViewBag.Message = "Invalid Username or Password";
+                    return View();
+                }
+                else 
+                { 
+                    return RedirectToAction("Index","Home");
+                }
+            }
+            ViewBag.Message = "Please fill all required fields";
+            return View();
+        }
+    }
+}
---
+++ Models/SigninModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Virtumart_MVC_3.Models
+{
+    public class SigninModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }        
+    }
+}
